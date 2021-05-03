6c5510a9d953c2aedc1a51c19a91947e7695b141
fgonzalezr1998 <fergonzaramos@yahoo.es>
2021-05-03 18:52:49 +0200

    [p3] changes added

--- at/practica03/js02.js
+++ at/practica03/js02.js
@@ -1,14 +1,47 @@
 'use strict'
 
-function conversor(n, output_units) {
-  if (output_units == undefined || !Array.isArray(output_units)) {
+const InchesPerMeter = 39.3701;
+const YardsPerMeter = 1.09361;
+
+function m2cm(n) {
+  return n * 100;
+}
+
+function m2in(n) {
+  return n * InchesPerMeter;
+}
+
+function m2yd(n) {
+  return n * YardsPerMeter;
+}
+
+function conversor(n, out_unit) {
+  let res;
+
+  if (out_unit == undefined) {
     return "Error: Bad arguments!";
   }
-  // Code this!
 
-  return "ok";
+  switch (out_unit) {
+    case "cm":
+      res = m2cm(n).toString();
+      break;
+    case "in":
+      res = m2in(n).toString();
+      break;
+    case "yd":
+      res = m2yd(n).toString();
+      break;
+    case "m":
+      res = n.toString();
+      break;
+    default:
+      res = "Error: Bad output unit!";
+  }
+
+  return res;
 }
 
 let units = ["m", "cm", "in", "yd"];
 
-console.log(conversor(2, units));
+console.log(conversor(2, ""));
