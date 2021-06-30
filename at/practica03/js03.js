acc78033482266b8f4241d7f7e7dad174fbd7fb2
fgonzalezr1998 <fergonzaramos@yahoo.es>
2021-06-30 14:43:08 +0200

    [p3] js03 psswd validator finished

--- at/practica03/js03.js
+++ at/practica03/js03.js
@@ -10,6 +10,20 @@ function is_int(min_long, error_msg)
   return Number.isInteger(min_long);
 }
 
+function is_num(c) {
+  return c.charCodeAt(0) >= '0'.charCodeAt(0) &&
+    c.charCodeAt(0) <= '9'.charCodeAt(0);
+}
+
+function is_special(c, special_chars) {
+  for (let i = 0; i < special_chars.length; i++) {
+    if (c == special_chars.charAt(i)) {
+      return true;
+    }
+  }
+  return false;
+}
+
 function args_ok(psswd, min_long, min_lowercase,
   min_uppercase, min_nums, min_special,
   special_chars, error_msg)
@@ -26,6 +40,15 @@ function args_ok(psswd, min_long, min_lowercase,
     ok = false;
   } else if (!is_int(min_uppercase)) {
     error_msg.msg = "Error: Minimum Number of Uppercase is not an integer";
+    ok = false;
+  } else if (!is_int(min_nums)) {
+    error_msg.msg = "Error: Minimum Numbers is not an integer";
+    ok = false;
+  } else if (!is_int(min_special)) {
+    error_msg.msg = "Error: Minimum Number of Special Characters is not an integer";
+    ok = false;
+  } else if (!is_string(special_chars)) {
+    error_msg.msg = "Error: Special Characters are not a string";
     ok = false;
   }
 
@@ -58,9 +81,56 @@ function min_lowercase_ok(psswd, min_lowercase, error_msg) {
   return true;
 }
 
+function min_uppercase_ok(psswd, min_uppercase, error_msg) {
+  let lower = psswd.toLowerCase();
+  console.log(lower)
+  let uppercases = 0;
+  for (let i = 0; i < psswd.length; i++) {
+    if (psswd.charAt(i) != lower.charAt(i)) {
+      uppercases++;
+    }
+  }
+
+  if (uppercases < min_uppercase) {
+    error_msg.msg = "Error: There are fewer upper case than required";
+    return false;
+  }
+  return true;
+}
+
+function min_nums_ok(psswd, min_nums, error_msg) {
+  let n_nums = 0;
+  for (let i = 0; i < psswd.length; i++) {
+    if (is_num(psswd.charAt(i))) {
+      n_nums++;
+    }
+  }
+
+  if (n_nums < min_nums) {
+    error_msg.msg = "Error: There are fewer number than required"
+    return false;
+  }
+
+  return true;
+}
+
+function special_chars_ok(psswd, min_special, special_chars, error_msg) {
+  let specials = 0;
+  for (let i = 0; i < psswd.length; i++) {
+    if(is_special(psswd.charAt(i), special_chars)) {
+      specials++;
+    }
+  }
+
+  if (specials < min_special) {
+    error_msg.msg = "Error: There are fewer special characters than required"
+    return false;
+  }
+  return true;
+}
+
 function psswd_validator(psswd, min_long, min_lowercase,
-  min_uppercase, min_nums, min_special,
-  special_chars)
+  min_uppercase, min_nums, min_special, special_chars)
 {
   let error_msg = {msg: ""};
   let ok = true;
@@ -78,8 +148,17 @@ function psswd_validator(psswd, min_long, min_lowercase,
   if (ok && !min_lowercase_ok(psswd, min_lowercase, error_msg)) {
     ok = false
   }
+  if (ok && !min_uppercase_ok(psswd, min_uppercase, error_msg)) {
+    ok = false
+  }
+  if (ok && !min_nums_ok(psswd, min_nums, error_msg)) {
+    ok = false
+  }
+  if (ok && !special_chars_ok(psswd, min_special, special_chars, error_msg)) {
+    ok = false
+  }
 
   return error_msg.msg;
 }
 
-console.log(psswd_validator("1234eeedooós", 4, 8, 1, 1, "%#_/"));
+console.log(psswd_validator("1234e#eedo/oós_", 4, 8, 0, 4, 3, "%#_/"));
